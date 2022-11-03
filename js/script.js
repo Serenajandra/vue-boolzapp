583dec522ae6e6a56e22c1b6c4fd3d262fcc37f3
Serenajandra <serena.jandra@gmail.com>
2022-11-03 16:22:06 +0100

    aggiunto label per inserire messaggi

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-// Il compito è di svolgere la milestone 1 del brief che vi invio in allegato:
-// -> Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// -> Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
+// Milestone 1
+// ●    Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+//      dall’interlocutore (bianco) assegnando due classi CSS diverse
+// [●]  Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+//      nome e immagine di ogni contatto
+// Milestone 2
+// [●]  Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+//      messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// [●]  Click sul contatto mostra la conversazione del contatto cliccato
+// Milestone 3
+// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+// Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
 
 const {createApp} = Vue;
 createApp({
     data(){
         return{
             activeItem: 0,
+            newUserMessage: "",
+
             contacts: [
                 {
                     name: 'Michele',
@@ -177,11 +194,21 @@ createApp({
  
     methods: {
 
-        showAvatar(){
-            this.contacts.visible = true
-            
+        selectItem(clickedAvatar){
+            this.activeItem = clickedAvatar  
         },
 
+        pushMessage(activeItem){
+            const userMessage = {
+                date: ``,
+                message: this.newUserMessage,
+                status: `sent`
+            };
+            this.contacts[activeItem].messages.push(userMessage);
+            
+            this.newUserMessage = "";
+        }
+        
     },
 
     
