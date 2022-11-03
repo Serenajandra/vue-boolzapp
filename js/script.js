ca280f045b6a9a8e732e74a76483c68f79d71706
Serenajandra <serena.jandra@gmail.com>
2022-11-03 18:11:02 +0100

    aggiunta risposta automatica nella chat

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@
 //      dall’interlocutore (bianco) assegnando due classi CSS diverse
 // [●]  Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
 //      nome e immagine di ogni contatto
+
 // Milestone 2
 // [●]  Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
 //      messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // [●]  Click sul contatto mostra la conversazione del contatto cliccato
+
 // Milestone 3
-// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
-// un “ok” come risposta, che apparirà dopo 1 secondo.
+// [●] Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+//     “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ●    Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+//      un “ok” come risposta, che apparirà dopo 1 secondo.
+
 // Milestone 4
 // ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
 // contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
@@ -189,24 +192,38 @@ createApp({
             ]
             
         }
-    },
-    
+    },  
  
     methods: {
 
         selectItem(clickedAvatar){
+            // Rendo visibile la chat dell' avatar cliccato
             this.activeItem = clickedAvatar  
         },
 
         pushMessage(activeItem){
+            // Creo l'oggetto che conterrà il messaggio dell'utente
             const userMessage = {
                 date: ``,
                 message: this.newUserMessage,
                 status: `sent`
             };
+            // Pusho il messaggio con l'oggetto che lo contiene
             this.contacts[activeItem].messages.push(userMessage);
-            
+            // Ripulisco il campo per scrivere
             this.newUserMessage = "";
+            
+            setTimeout(() => {
+                 // Creo l'oggetto che conterrà la risposta al messaggio delll'utente
+                 const answer = {
+                    date: '',
+                    message: 'ok sweety!',
+                    status: 'received'
+                };
+                // pusho l'ggetto contenente la risposta
+                this.contacts[activeItem].messages.push(answer);
+            
+            }, 1000);
         }
         
     },
