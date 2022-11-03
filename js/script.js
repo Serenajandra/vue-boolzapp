ae2a2fc7f34b9aeb5196486cab2c9e79b9d0c04a
Serenajandra <serena.jandra@gmail.com>
2022-11-03 19:27:35 +0100

    iniziato refactoring di index

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 // Milestone 3
 // [●] Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
 //     “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// ●    Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// [●]  Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 //      un “ok” come risposta, che apparirà dopo 1 secondo.
 
 // Milestone 4
