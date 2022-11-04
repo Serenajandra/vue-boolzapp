82863ced0875ed4f038f5e02ac912da1a4f33525
Serenajandra <serena.jandra@gmail.com>
2022-11-04 16:44:03 +0100

    completata milestone 4

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@
 //      un “ok” come risposta, che apparirà dopo 1 secondo.
 
 // Milestone 4
-// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
-// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
-// “mar” rimangono solo Marco e Martina)
+// [●] Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+//     contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+//     “mar” rimangono solo Marco e Martina)
 
 const {createApp} = Vue;
 const dt = luxon.DateTime;
